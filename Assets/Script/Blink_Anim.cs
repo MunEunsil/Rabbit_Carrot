431f481b4f8381629f9a6d5b6ac404be875e5f6f
MunEunsil <muneunsil1220@gmail.com>
2020-04-03 00:16:53 +0900

    sksmsqkqh

---
+++ Assets/Script/Blink_Anim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blink_Anim : MonoBehaviour
+{
+    float time;
+
+    private void Update()
+    {
+        if(time < 0.5f)
+        {
+           // GetComponent<Image>().color = new Color(1, 1, 1, 1 - time);
+           GetComponent<Text>().color = new Color(1, 1, 1, 1 - time);
+        }
+        else
+        {
+            // GetComponent<Image>().color = new Color(1, 1, 1, time);
+            GetComponent<Text>().color = new Color(1, 1, 1, time);
+            if (time > 1f)
+            {
+                time = 0;
+            }
+        }
+        time += Time.deltaTime;
+    }
+
+}
